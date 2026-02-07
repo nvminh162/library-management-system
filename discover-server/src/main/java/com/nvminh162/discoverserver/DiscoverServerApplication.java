1c2007aafc41c7c71f25d06bd3d834d448017a62
nvminh162 <nvminh162@gmail.com>
2026-02-07 09:51:30 +0700

    Initialize DiscoverServer Spring Boot microservice with Maven wrapper, Eureka server configuration, and essential setup files

---
+++ discover-server/src/main/java/com/nvminh162/discoverserver/DiscoverServerApplication.java
@@ -0,0 +1,13 @@
+package com.nvminh162.discoverserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverServerApplication.class, args);
+	}
+
+}
