8397c5216ac2fec4b25da79e6004592d0febed12
Nguyen Van Minh <nvminh162@gmail.com>
2026-02-28 21:56:04 +0700

    test CI CD 7

--- discover-server/src/main/java/com/nvminh162/discoverserver/DiscoverServerController.java
+++ discover-server/src/main/java/com/nvminh162/discoverserver/DiscoverServerController.java
@@ -10,6 +10,6 @@ public class DiscoverServerController {
 
     @GetMapping
     public String time(){
-        return "(1) Current date time: " + java.time.LocalDateTime.now();
+        return "(2) Current date time: " + java.time.LocalDateTime.now();
     }
 }
